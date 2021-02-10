4bf737c7359a7f162923e0defe20b5bdf8f335ef
Saagar <saagar.mahadev123@gmail.com>
2021-02-09 20:33:57 -0800

    Push to heroku

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-const uri = 'mongodb+srv://brutrition:UCLAbru2021@brutrition.qemvx.mongodb.net/Prod?retryWrites=true&w=majority' || process.env.MONGOLAB_URI;
+const uri = /*'mongodb+srv://brutrition:UCLAbru2021@brutrition.qemvx.mongodb.net/Prod?retryWrites=true&w=majority' ||*/ process.env.MONGOLAB_URI;
 
 mongoose.connect(uri, {
 	useNewUrlParser: true,
